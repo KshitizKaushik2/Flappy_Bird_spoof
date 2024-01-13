bc908a66348713322be6753037465d1dceb922aa
kshitiz sharma <107552525+KshitizKaushik2@users.noreply.github.com>
2024-01-13 22:59:34 +0530

    Add files via upload

---
+++ flappy bird/Bird.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float upForce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2D.velocity = Vector2.zero;
+                rb2D.AddForce(new Vector2(0, upForce));
+            }
+        }
+    }
+
+    void OnCollisionEnter2D()
+    {
+        isDead = true;
+    }
+}
